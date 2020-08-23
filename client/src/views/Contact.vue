07964d282113c1e595608af19f009876281e33b9
Gusman Widodo <gusmanwidodo@gmail.com>
2020-08-23 11:01:22 +0700

    [CLIENT] Add contact view
    Signed-off-by: Gusman Widodo <gusmanwidodo@gmail.com>

---
+++ client/src/views/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <a href="https://www.linkedin.com/in/gusmanwidodo/"
+        >https://www.linkedin.com/in/gusmanwidodo/</a
+      >
+    </div>
+    <div>
+      <a href="mailto:gusmanwidodo@gmail.com">gusmanwidodo@gmail.com</a>
+    </div>
+  </div>
+</template>
